1f0df19684ffae9054bc3fbd50a02ebcef0b4a01
Eugene <yuting0501@gmail.com>
2020-03-27 21:08:07 +0800

    Add wifi initialization and choose to use communication interface by PLATFORM_ID

---
+++ main/platforms.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2020 Particle Industries, Inc.  All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define PLATFORM_GCC                        3
+#define PLATFORM_PHOTON_DEV                 4
+#define PLATFORM_TEACUP_PIGTAIL_DEV         5
+#define PLATFORM_PHOTON_PRODUCTION          6
+#define PLATFORM_TEACUP_PIGTAIL_PRODUCTION  7
+#define PLATFORM_P1                         8
+#define PLATFORM_ETHERNET_PROTO             9
+#define PLATFORM_ELECTRON_PRODUCTION        10
+#define PLATFORM_ARGON                      12
+#define PLATFORM_BORON                      13
+#define PLATFORM_XENON                      14
+#define PLATFORM_ASOM                       22
+#define PLATFORM_BSOM                       23
+#define PLATFORM_B5SOM                      25
+#define PLATFORM_TRACKER                    26
+#define PLATFORM_NEWHAL                     60000
--- platform.mk
+++ platform.mk
@@ -6,8 +6,10 @@ endif
 
 ifeq ("$(PLATFORM)","argon")
 export ESP_AT_MODULE_NAME ?= ARGON
+CXXFLAGS += -DPLATFORM_ID=12
 else ifeq ("$(PLATFORM)","atsom")
 export ESP_AT_MODULE_NAME ?= ATSOM
+CXXFLAGS += -DPLATFORM_ID=26
 endif
 
 $(info Build for platform: $(PLATFORM))
