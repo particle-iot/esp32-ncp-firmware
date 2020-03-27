0527feec1d08c4726d42ffba140ebbb9731e29b6
Eugene <yuting0501@gmail.com>
2020-03-27 21:14:11 +0800

    Rename atsom with tracker

--- platform.mk
+++ platform.mk
@@ -1,14 +1,14 @@
 # Default platform
 ifeq ("$(PLATFORM)","")
-PLATFORM := atsom
+PLATFORM := tracker
 $(info Use default PLATFORM: $(PLATFORM))
 endif
 
 ifeq ("$(PLATFORM)","argon")
 export ESP_AT_MODULE_NAME ?= ARGON
 CXXFLAGS += -DPLATFORM_ID=12
-else ifeq ("$(PLATFORM)","atsom")
-export ESP_AT_MODULE_NAME ?= ATSOM
+else ifeq ("$(PLATFORM)","tracker")
+export ESP_AT_MODULE_NAME ?= TRACKER
 CXXFLAGS += -DPLATFORM_ID=26
 endif
 
